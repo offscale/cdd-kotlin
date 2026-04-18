8cb77b7148a640e6d984e68d9e24a14f76bbc56c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-04-18 20:33:39 +1200

    CI fixes

--- src/wasmMain/kotlin/org/cdd/wasm/Main.kt
+++ src/wasmMain/kotlin/org/cdd/wasm/Main.kt
@@ -13,18 +13,27 @@ fun processWasmArgs(args: Array<String>): String {
     return "cdd-kotlin WASM received: " + args.joinToString(", ")
 }
 
+/**
+ * Exported WASM function for from_openapi.
+ */
 @OptIn(kotlin.wasm.ExperimentalWasmInterop::class)
 @WasmExport
 fun from_openapi(): Int {
     return runCli(arrayOf("from_openapi"))
 }
 
+/**
+ * Exported WASM function for to_openapi.
+ */
 @OptIn(kotlin.wasm.ExperimentalWasmInterop::class)
 @WasmExport
 fun to_openapi(): Int {
     return runCli(arrayOf("to_openapi"))
 }
 
+/**
+ * Exported WASM function for to_docs_json.
+ */
 @OptIn(kotlin.wasm.ExperimentalWasmInterop::class)
 @WasmExport
 fun to_docs_json(): Int {
